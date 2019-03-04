36260fe03afac9df334fcf018d91c533eb4de8a7
Caleb Heuer <caleb.heuer@gmail.com>
2019-03-03 23:02:25 -0700

    Add comments

--- control.lua
+++ control.lua
@@ -1,11 +1,13 @@
 script.on_event(defines.events.on_lua_shortcut, function(e)
 
-    if e.prototype_name == 'tapeline' then 
+    if e.prototype_name == 'tapeline' then
 
+        -- setup constants
         local player = game.players[e.player_index]
 
+        -- clean player cursor, and insert tapeline item (for debugging purposes, insert blueprint)
         player.clean_cursor()
-        player.cursor_stack.set_stack({name = 'blueprint-book'})
+        player.cursor_stack.set_stack({name = 'blueprint'})
 
     end
 
