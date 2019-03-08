5f836e59c3a4c3e1a8e28532999d5d5370ca8b29
Caleb Heuer <caleb.heuer@gmail.com>
2019-03-07 20:35:07 -0700

    Adjust grid colors again

--- constants.lua
+++ constants.lua
@@ -21,11 +21,11 @@ constants.isDebugMode = true
 constants.colors = {}
 constants.colors.tilegrid_div = {}
 
-constants.colors.tilegrid_div[1] = {r=0.3,g=0.3,b=0.7,a=0.4}
-constants.colors.tilegrid_div[5] = {r=0.3,g=0.8,b=0.3,a=0.4}
-constants.colors.tilegrid_div[25] = {r=0.9,g=0.3,b=0.3,a=0.4}
-constants.colors.tilegrid_div[100] = {r=0.8,g=0.8,b=0.3,a=0.4}
-constants.colors.tilegrid_border = {r=0.7,g=0.7,b=0.7,a=0.4}
+constants.colors.tilegrid_div[1] = {r=0.3,g=0.3,b=0.7,a=0.5}
+constants.colors.tilegrid_div[5] = {r=0.3,g=0.8,b=0.3,a=0.5}
+constants.colors.tilegrid_div[25] = {r=0.9,g=0.3,b=0.3,a=0.5}
+constants.colors.tilegrid_div[100] = {r=0.8,g=0.8,b=0.3,a=0.5}
+constants.colors.tilegrid_border = {r=0.7,g=0.7,b=0.7,a=0.5}
 constants.colors.tilegrid_label = {r=0.7,g=0.7,b=0.7,a=0.8}
 
 -- positioning / graphics
