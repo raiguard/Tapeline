0bcab21d843e207a3ee3b654854800786256b92f
Caleb Heuer <caleb.heuer@gmail.com>
2019-03-07 21:18:35 -0700

    Final color tweaks

--- constants.lua
+++ constants.lua
@@ -15,17 +15,18 @@ constants.scriptAssetPath = constants.modAssetName .. '/graphics/icons/script/'
 
 constants.stdlib = '__stdlib__/stdlib/'
 
-constants.isDebugMode = true
+constants.isDebugMode = false
 
 -- colors
 constants.colors = {}
 constants.colors.tilegrid_div = {}
 
-constants.colors.tilegrid_div[1] = {r=0.3,g=0.3,b=0.7,a=0.5}
-constants.colors.tilegrid_div[5] = {r=0.3,g=0.8,b=0.3,a=0.5}
-constants.colors.tilegrid_div[25] = {r=0.9,g=0.3,b=0.3,a=0.5}
-constants.colors.tilegrid_div[100] = {r=0.8,g=0.8,b=0.3,a=0.5}
-constants.colors.tilegrid_border = {r=0.7,g=0.7,b=0.7,a=0.5}
+constants.colors.tilegrid_div[1] = {r=0.3,g=0.3,b=0.7,a=0.7}
+constants.colors.tilegrid_div[5] = {r=0.3,g=0.8,b=0.3,a=0.7}
+constants.colors.tilegrid_div[25] = {r=0.9,g=0.3,b=0.3,a=0.7}
+constants.colors.tilegrid_div[100] = {r=0.8,g=0.8,b=0.3,a=0.7}
+constants.colors.tilegrid_background = {r=0,g=0,b=0,a=0.4}
+constants.colors.tilegrid_border = {r=0.6,g=0.6,b=0.6,a=1}
 constants.colors.tilegrid_label = {r=0.7,g=0.7,b=0.7,a=0.8}
 
 -- positioning / graphics
