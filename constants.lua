f3f88ce719a4cb5e30968b7029ce7b69fc7d59a6
Caleb Heuer <caleb.heuer@gmail.com>
2019-03-07 19:22:12 -0700

    Add tile grid v2

--- constants.lua
+++ constants.lua
@@ -17,4 +17,14 @@ constants.stdlib = '__stdlib__/stdlib/'
 
 constants.isDebugMode = true
 
+-- colors
+constants.colors = {}
+
+constants.colors.tilegrid_div_1 = {r=0.3,g=0.3,b=0.7,a=0.2}
+constants.colors.tilegrid_div_5 = {r=0.3,g=0.8,b=0.3,a=0.2}
+constants.colors.tilegrid_div_25 = {r=0.9,g=0.3,b=0.3,a=0.2}
+constants.colors.tilegrid_div_100 = {r=0.3,g=0.3,b=0.7,a=0.2}
+constants.colors.tilegrid_border = {r=0.7,g=0.7,b=0.7,a=0.2}
+constants.colors.tilegrid_label = {r=0.7,g=0.7,b=0.7,a=0.8}
+
 return constants
--- control.lua
+++ control.lua
@@ -41,34 +41,33 @@ function measure_area(e)
     Logger.log(area)
 
     -- draw tile grid
-    for i=0,area.height do
-        rendering.draw_line{color={r=0.5,g=0.5,b=0.5,a=0.000000001}, width=1, from={(area.left_top.x),(area.left_top.y + i)}, to={area.right_top.x,(area.left_top.y + i)}, surface=surfaceIndex, time_to_live=180, draw_on_ground=1}
+    rendering.draw_rectangle{color=constants.colors.tilegrid_border, width=1.2, filled=false, left_top={area.left_top.x,area.left_top.y}, right_bottom={area.right_bottom.x,area.right_bottom.y}, surface=surfaceIndex, time_to_live=180, draw_on_ground=1}
+    for i=1,area.height do
+        rendering.draw_line{color=constants.colors['tilegrid_div_' .. (i % 100 == 0 and 100 or (i % 25 == 0 and 25 or (i % 5 == 0 and 5 or 1)))], width=1.2, from={(area.left_top.x),(area.left_top.y + i)}, to={area.right_top.x,(area.left_top.y + i)}, surface=surfaceIndex, time_to_live=180, draw_on_ground=1}
     end
 
-    for i=0,area.width do
-        rendering.draw_line{color={r=0.5,g=0.5,b=0.5,a=0.000000001}, width=1, from={(area.left_top.x + i),area.left_top.y}, to={(area.left_top.x + i),area.right_bottom.y}, surface=surfaceIndex, time_to_live=180, draw_on_ground=1}
+    for i=1,area.width do
+        rendering.draw_line{color=constants.colors['tilegrid_div_' .. (i % 100 == 0 and 100 or (i % 25 == 0 and 25 or (i % 5 == 0 and 5 or 1)))], width=1.2, from={(area.left_top.x + i),area.left_top.y}, to={(area.left_top.x + i),area.right_bottom.y}, surface=surfaceIndex, time_to_live=180, draw_on_ground=1}
     end
 
     -- draw edge rulers
-    if area.height > 1 and area.width > 1 then rendering.draw_rectangle{color={r=0.55,g=0.55,b=0.55,a=1}, filled=true, left_top={(area.left_top.x - 0.2),(area.left_top.y - 0.2)}, right_bottom={(area.left_top.x),(area.left_top.y)}, surface=surfaceIndex, time_to_live=180} end
+    -- if area.height > 1 and area.width > 1 then rendering.draw_rectangle{color={r=0.55,g=0.55,b=0.55,a=1}, filled=true, left_top={(area.left_top.x - 0.2),(area.left_top.y - 0.2)}, right_bottom={(area.left_top.x),(area.left_top.y)}, surface=surfaceIndex, time_to_live=180} end
 
     if area.height > 1 then
-        rendering.draw_text{text=area.height, surface=surfaceIndex, target={(area.left_top.x - 1.8), area.midpoints.y}, color={r=0.65,g=0.65,b=0.65}, alignment='center', scale=3, orientation=0.75, time_to_live=180}
-        for i=0,(area.height - 1) do
-            local c = ((i % 2 == 0) and 0.3 or 0.55)
-            rendering.draw_rectangle{color={r=c,g=c,b=c,a=1}, filled=true, left_top={(area.left_top.x - 0.2),(area.left_top.y + i)}, right_bottom={(area.left_top.x),(area.left_top.y + (i + 1))}, surface=surfaceIndex, time_to_live=180}
-        end
+        rendering.draw_text{text=area.height, surface=surfaceIndex, target={(area.left_top.x - 1.2), area.midpoints.y}, color=constants.colors.tilegrid_label, alignment='center', scale=2, orientation=0.75, time_to_live=180}
+        -- for i=0,(area.height - 1) do
+        --     local c = ((i % 2 == 0) and 0.3 or 0.55)
+        --     rendering.draw_rectangle{color={r=c,g=c,b=c,a=1}, filled=true, left_top={(area.left_top.x - 0.2),(area.left_top.y + i)}, right_bottom={(area.left_top.x),(area.left_top.y + (i + 1))}, surface=surfaceIndex, time_to_live=180}
+        -- end
     end
 
     if area.width > 1 then
-        rendering.draw_text{text=area.width, surface=surfaceIndex, target={area.midpoints.x, (area.left_top.y - 1.8)}, color={r=0.65,g=0.65,b=0.65}, alignment='center', scale=3, time_to_live=180}
-        for i=0,(area.width - 1) do
-            local c = ((i % 2 == 0) and 0.3 or 0.55)
-            rendering.draw_rectangle{color={r=c,g=c,b=c,a=1}, filled=true, left_top={(area.left_top.x + i),(area.left_top.y - 0.2)}, right_bottom={(area.left_top.x + (i + 1)),(area.left_top.y)}, surface=surfaceIndex, time_to_live=180}
-        end
+        rendering.draw_text{text=area.width, surface=surfaceIndex, target={area.midpoints.x, (area.left_top.y - 1.2)}, color=constants.colors.tilegrid_label, alignment='center', scale=2, time_to_live=180}
+        -- for i=0,(area.width - 1) do
+        --     local c = ((i % 2 == 0) and 0.3 or 0.55)
+        --     rendering.draw_rectangle{color={r=c,g=c,b=c,a=1}, filled=true, left_top={(area.left_top.x + i),(area.left_top.y - 0.2)}, right_bottom={(area.left_top.x + (i + 1)),(area.left_top.y)}, surface=surfaceIndex, time_to_live=180}
+        -- end
     end
-
-    
 
 end
 
