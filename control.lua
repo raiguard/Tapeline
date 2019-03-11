caaacf9408c9b950c03224daecca9da260c056c8
Caleb Heuer <caleb.heuer@gmail.com>
2019-03-11 16:07:04 -0600

    Rewrite shortcut prototype to properly handle item

--- control.lua
+++ control.lua
@@ -4,22 +4,11 @@ local Event = require('__stdlib__/stdlib/event/event')
 local Area = require('__stdlib__/stdlib/area/area')
 local Logger = require('__stdlib__/stdlib/misc/logger').new('Tapeline', 'Tapeline_Debug', constants.isDebugMode)
 
-function create_tapeline(player)
-    player.clean_cursor()
-    player.cursor_stack.set_stack({name = 'tapeline-tool'})
-end
-
 function on_custom_input(e)
 
-    create_tapeline(game.players[e.player_index])
-
-end
-
-function on_shortcut_pressed(e)
-
-    if e.prototype_name ~= 'tapeline-shortcut' then return end
-
-    create_tapeline(game.players[e.player_index])
+    local player = game.players[e.player_index]
+    player.clean_cursor()
+    player.cursor_stack.set_stack({name = 'tapeline-tool'})
 
 end
 
@@ -163,26 +152,5 @@ function measure_area(e)
 
 end
 
-function cleanup_tapeline_tool(e)
-
-    local player = game.players[e.player_index]
-    local is_trash = e.name == defines.events.on_player_trash_inventory_changed
-    local inventory
-
-    if is_trash then inventory = player.get_inventory(defines.inventory.player_trash)
-    elseif is_trash == false then inventory = player.get_main_inventory()
-    else return
-    end
-
-    local tapeline_tool = game.item_prototypes['tapeline-tool'] and inventory.find_item_stack('tapeline-tool')
-        
-    if tapeline_tool then return tapeline_tool.clear() end
-
-    return
-
-end
-
 Event.register('get-tapeline-tool', on_custom_input)
-Event.register(defines.events.on_lua_shortcut, on_shortcut_pressed)
 Event.register({defines.events.on_player_selected_area, defines.events.on_player_alt_selected_area}, measure_area)
-Event.register({defines.events.on_player_main_inventory_changed, defines.events.on_player_trash_inventory_changed}, cleanup_tapeline_tool)
