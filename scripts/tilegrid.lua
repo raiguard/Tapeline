56308baf7d23a9f54358b90ada90fdc2c60ec304
Caleb Heuer <caleb.heuer@gmail.com>
2019-06-18 15:56:02 -0600

    Get alternate tilegrid working

--- scripts/tilegrid.lua
+++ scripts/tilegrid.lua
@@ -50,7 +50,7 @@ function construct_tilegrid_data(e)
     data.origin = stdlib.position.add(data.area.left_top, { x = 0.5, y = 0.5 })
     data.time_of_creation = game.ticks_played
     data.time_to_live = data.owner_settings.tilegrid_clear_delay
-    data.grid_type = 0
+    data.grid_type = 1
     -- tilegrid divisors
 	data.tilegrid_divisors = {}
 	if data.grid_type == 1 then
@@ -81,6 +81,11 @@ function update_tilegrid_data(e)
     data.area.size,data.area.width,data.area.height = data.area:size()
     data.area.midpoints = stdlib.area.center(data.area)
     global[global.cur_tilegrid_index] = data
+    -- tilegrid divisors
+    if data.grid_type == 1 then
+        data.tilegrid_divisors[2] = { x = (data.area.width > 1 and (data.area.width / data.owner_settings.tilegrid_split_divisor) or data.area.width), y = (data.area.height > 1 and (data.area.height / data.owner_settings.tilegrid_split_divisor) or data.area.height) }
+        data.tilegrid_divisors[3] = { x = (data.area.width > 1 and (data.area.midpoints.x - data.area.left_top.x) or data.area.width), y = (data.area.height > 1 and (data.area.midpoints.y - data.area.left_top.y) or data.area.height) }
+    end
     -- update render objects
     destroy_render_objects(global[global.cur_tilegrid_index].render_objects)
     global[global.cur_tilegrid_index].render_objects = build_render_objects(global[global.cur_tilegrid_index])
