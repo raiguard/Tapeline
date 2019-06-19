c9b89c1aa384401d0e7c5ceb12dee4d2ac55c78e
raiguard <caleb.heuer@gmail.com>
2019-06-19 00:21:48 -0600

    Update icons

--- Tapeline/prototypes/item.lua
+++ Tapeline/prototypes/item.lua
@@ -1,34 +1,15 @@
 data:extend({
   {
-      type = 'selection-tool',
-      name = 'tapeline-tool',
-      icon = '__Tapeline__/graphics/icons/item/tapeline-tool.png',
-      icon_size = 32,
-      flags = {'hidden', 'only-in-cursor'},
-      subgroup = 'other',
-      order = 'c[automated-construction]-a[blueprint]',
-      stack_size = 1,
-      stackable = false,
-      selection_color = { g = 1 },
-      alt_selection_color = { g = 1, b = 1 },
-      selection_mode = {'any-tile'},
-      alt_selection_mode = {'any-tile'},
-      selection_cursor_box_type = 'copy',
-      alt_selection_cursor_box_type = 'electricity',
-      show_in_library = false
-  },
-  {
     type = 'capsule',
     name = 'tapeline-capsule',
     icon = '__Tapeline__/graphics/icons/item/tapeline-tool.png',
-    icon_size = 32,
+    icon_size = 64,
     subgroup = 'capsule',
     order = 'zz',
+    flags = {'hidden', 'only-in-cursor'},
     stack_size = 10000,
-    flags = {'hidden'},
-    -- When 0.17.51 comes out:
-    -- stack_size = 1,
-    -- uses_stack = false,
+    -- stackable = false,
+    uses_stack = false,
     capsule_action = {
       type = 'throw',
       attack_parameters =
