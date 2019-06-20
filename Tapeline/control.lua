d2d8a5477199daea8da9c4c4953e4ed9c5fcdd10
Caleb Heuer <caleb.heuer@gmail.com>
2019-06-20 13:05:22 -0600

    Play with GUIs some more

--- Tapeline/control.lua
+++ Tapeline/control.lua
@@ -2,6 +2,7 @@ stdlib = {}
 stdlib.area = require('__stdlib__/stdlib/area/area')
 stdlib.color = require('__stdlib__/stdlib/utils/color')
 stdlib.event = require('__stdlib__/stdlib/event/event')
+stdlib.gui = require('__stdlib__/stdlib/event/gui')
 stdlib.logger = require('__stdlib__/stdlib/misc/logger').new('Tapeline_Debug', true)
 stdlib.position = require('__stdlib__/stdlib/area/position')
 stdlib.table = require('__stdlib__/stdlib/utils/table')
--- Tapeline/scripts/gui.lua
+++ Tapeline/scripts/gui.lua
@@ -1,45 +1,102 @@
+-- create a menu for the player
+function create_menu(player, flow)
+
+    flow.add {
+        type = 'frame',
+        name = 'tapeline_menu_frame',
+        direction = 'vertical'
+    }
+
+    flow.tapeline_menu_frame.add {
+        type = 'table',
+        name = 'settings_table',
+        column_count = 2
+    }
+
+    local settings_table = flow.tapeline_menu_frame.settings_table
+
+    -- -- AUTO CLEAR
+    -- settings_table.add {
+    --     type = 'label',
+    --     name = 'autoclear_label',
+    --     caption = 'Auto clear',
+    --     style = 'bold_label'
+    -- }
+    -- settings_table.add {
+    --     type = 'checkbox',
+    --     name = 'autoclear_checkbox',
+    --     state = true
+    -- }
+
+    -- GRID TYPE
+    settings_table.add {
+        type = 'label',
+        name = 'grid_type_label',
+        caption = 'Grid type',
+        style = 'bold_label'
+    }
+
+    settings_table.add {
+        type = 'drop-down',
+        name = 'grid_type_dropdown',
+        items = { 'increment', 'split' },
+        selected_index = 1
+    }
+
+    -- INCREMENT DIVISOR
+    settings_table.add {
+        type = 'label',
+        name = 'increment_divisor_label',
+        caption = 'Increment Divisor',
+        style = 'bold_label'
+    }
+    settings_table.add {
+        type = 'textfield',
+        name = 'increment_divisor_textfield',
+        text = '5'
+    }
+    settings_table.increment_divisor_textfield.style.width = 40
+
+    -- SPLIT DIVISOR
+    settings_table.add {
+        type = 'label',
+        name = 'split_divisor_label',
+        caption = 'Split Divisor',
+        style = 'bold_label'
+    }
+    settings_table.add {
+        type = 'textfield',
+        name = 'split_divisor_textfield',
+        text = '4'
+    }
+    settings_table.split_divisor_textfield.style.width = 40
+
+end
+
+-- show or create the menu for the player
 function show_menu(player)
 
     local flow = mod_gui.get_frame_flow(player)
-    local container = flow.tapeline_menu_container
-    local menu_frame = flow.tapeline_mainmenu_frame
-    if not container then
-        container = flow.add {
-            type = 'flow',
-            name = 'tapeline_menu_container',
-            direction = 'horizontal'
-        }
-        menu_frame = container.add {
-            type = 'frame',
-            name = 'tapeline_menu_frame',
-            direction = 'vertical',
-            caption = 'Tapeline'
-        }
-
-        menu_frame.add {
-            type = 'label',
-            name = 'tapeline_menu_button',
-            caption = 'Grid type:',
-            style = 'subheader_caption_label'
-        }
-
-        stdlib.logger.log(game.surfaces)
-
+    local menu_frame = flow.tapeline_menu_frame
+    if not menu_frame then
+       create_menu(player, flow)
     else
-        container.visible = true
+        menu_frame.visible = true
     end
 
 end
 
+-- hide the menu for the player
 function hide_menu(player)
 
-    local container = mod_gui.get_frame_flow(player).tapeline_menu_container
-    if container then
-        container.visible = false
+    local menu_frame = mod_gui.get_frame_flow(player).tapeline_menu_frame
+    if menu_frame then
+        menu_frame.visible = false
     end
 
 end
 
+-- detect if the player is holding the tapeline capsule
 function on_item(e)
 
     local player = game.players[e.player_index]
@@ -51,5 +108,12 @@ function on_item(e)
     end
 
 end
+
+function on_dropdown_changed(e)
+    stdlib.logger.log(e)
+    game.print('Player ' .. game.players[e.player_index].name .. 'changed dropdown to ' .. e.element.items[e.element.selected_index])
+end
+
+stdlib.gui.on_selection_state_changed('grid_type_dropdown', on_dropdown_changed)
 
 stdlib.event.register(defines.events.on_player_cursor_stack_changed, on_item)
