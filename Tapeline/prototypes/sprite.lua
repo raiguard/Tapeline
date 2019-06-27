d47b0aae91883756ce9c71c94566b0e6f40bd069
raiguard <caleb.heuer@gmail.com>
2019-06-27 15:58:52 -0600

    Code cleanup

--- Tapeline/prototypes/sprite.lua
+++ Tapeline/prototypes/sprite.lua
@@ -1,14 +1,7 @@
 data:extend({
     {
         type = 'sprite',
-        name = 'trash-black',
-        filename = '__core__/graphics/icons/trash.png',
-        size = 32,
-        flags = {'icon'}
-    },
-    {
-        type = 'sprite',
-        name = 'check-mark',
+        name = 'check_mark',
         filename = '__Tapeline__/graphics/icons/gui/check-mark.png',
         size = 32,
         flags = {'icon'}
