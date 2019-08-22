f38464185736c129cbae2b71d02cf7fc788d6d16
raiguard <caleb.heuer@gmail.com>
2019-08-22 14:28:24 -0600

    Fix crash when adding the mod to an existing save

--- src/migrations/0.6.0.lua
+++ src/migrations/0.6.0.lua
@@ -14,15 +14,17 @@ for k,v in pairs(global) do
     end
 end
 -- rename players table
-global.players = table.deepcopy(global.player_data)
+global.players = table.deepcopy(global.player_data or {})
 global.player_data = nil
 
 for i,p in pairs(game.players) do
-    -- remove old settings window if it's open
-    local window = mod_gui.get_frame_flow(p).tapeline_menu_frame
-    if window then window.destroy() end
-    -- convert cur_editing from bool to number
-    global.players[i].cur_editing = 0
-    -- add cur_drawing
-    global.players[i].cur_drawing = 0
+    if global.players[i] then
+        -- remove old settings window if it's open
+        local window = mod_gui.get_frame_flow(p).tapeline_menu_frame
+        if window then window.destroy() end
+        -- convert cur_editing from bool to number
+        global.players[i].cur_editing = 0
+        -- add cur_drawing
+        global.players[i].cur_drawing = 0
+    end
 end
