c2a15a45560045d0f3ed42a721485f0e190b5892
raiguard <caleb.heuer@gmail.com>
2020-01-22 23:43:43 -0700

    Fix up settings

--- src/settings.lua
+++ src/settings.lua
@@ -2,79 +2,79 @@ data:extend{
     -- player settings
     {
         type = 'bool-setting',
+        name = 'log-selection-area',
+        setting_type = 'runtime-per-user',
+        default_value = false,
+        order = 'a'
+    },
+    {
+        type = 'bool-setting',
         name = 'draw-tilegrid-on-ground',
         setting_type = 'runtime-per-user',
         default_value = true,
-        order = 'a'
+        order = 'b'
     },
     {
         type = 'double-setting',
         name = 'tilegrid-line-width',
         setting_type = 'runtime-per-user',
         default_value = 1.5,
-        order = 'b'
+        order = 'c'
     },
     {
         type = 'double-setting',
         name = 'tilegrid-clear-delay',
         setting_type = 'runtime-per-user',
         default_value = 1,
-        order = 'c'
+        order = 'd'
     },
     {
         type = 'string-setting',
         name = 'tilegrid-background-color',
         setting_type = 'runtime-per-user',
         default_value = '{a=0.8}',
-        order = 'da'
+        order = 'ea'
     },
     {
         type = 'string-setting',
         name = 'tilegrid-border-color',
         setting_type = 'runtime-per-user',
         default_value = '{r=0.8, g=0.8, b=0.8}',
-        order = 'db'
+        order = 'eb'
     },
     {
         type = 'string-setting',
         name = 'tilegrid-label-color',
         setting_type = 'runtime-per-user',
         default_value = '{r=0.8, g=0.8, b=0.8}',
-        order = 'dc'
+        order = 'ec'
     },
     {
         type = 'string-setting',
         name = 'tilegrid-color-1',
         setting_type = 'runtime-per-user',
         default_value = '{r=0.5, g=0.5, b=0.5}',
-        order = 'dd'
+        order = 'ed'
     },
     {
         type = 'string-setting',
         name = 'tilegrid-color-2',
         setting_type = 'runtime-per-user',
         default_value = '{r=0.4, g=0.8, b=0.4}',
-        order = 'de'
+        order = 'ee'
     },
     {
         type = 'string-setting',
         name = 'tilegrid-color-3',
         setting_type = 'runtime-per-user',
         default_value = '{r=0.8, g=0.3, b=0.3}',
-        order = 'df'
+        order = 'ef'
     },
     {
         type = 'string-setting',
         name = 'tilegrid-color-4',
         setting_type = 'runtime-per-user',
         default_value = '{r=0.8, g=0.8, b=0.3}',
-        order = 'dg'
-    },
-    {
-        type = 'bool-setting',
-        name = 'log-selection-area',
-        setting_type = 'runtime-per-user',
-        default_value = false,
-        order = 'a'
+        order = 'eg'
     }
 }
