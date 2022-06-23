692b550a9819c0729a8a03956ce0b88ee2b1c5b7
Caleb Heuer <ch@raiguard.me>
2022-06-22 22:50:53 -0600

    Properly set the not-selectable-in-game entity flag

--- src/prototypes/entity.lua
+++ src/prototypes/entity.lua
@@ -5,9 +5,10 @@ data:extend({
   {
     type = "simple-entity-with-force",
     name = "tl-dummy-entity",
-    flags = { "not-on-map", "hidden", "not-selectable-in-game", "player-creation" },
+    flags = { "not-on-map", "hidden", "player-creation" },
     collision_box = { { -0.4, -0.4 }, { 0.4, 0.4 } },
     collision_mask = { collision_mask_util.get_first_unused_layer() },
+    selectable_in_game = false,
     picture = {
       filename = data_util.empty_image,
       width = 8,
