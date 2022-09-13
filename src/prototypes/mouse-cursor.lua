73b63854fbe72527ec29c2d853bf341cbd51e501
Caleb Heuer <ch@raiguard.me>
2022-09-13 01:16:55 -0600

    Consolidate all data into one file

--- src/prototypes/mouse-cursor.lua
+++
@@ -1,9 +0,0 @@
-data:extend({
-  {
-    type = "mouse-cursor",
-    name = "tl-tool-cursor",
-    filename = "__Tapeline__/graphics/cursor/draw-cursor.png",
-    hot_pixel_x = 1,
-    hot_pixel_y = 1,
-  },
-})
