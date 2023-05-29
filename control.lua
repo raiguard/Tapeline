163adf75285dcc2aa76ac054925bb255945a7c94
Caleb Heuer <ch@raiguard.me>
2023-05-29 11:30:10 +0200

    Move scripts into subdirectory

--- control.lua
+++ control.lua
@@ -1,4 +1,4 @@
 local handler = require("__core__/lualib/event_handler")
 
-handler.add_lib(require("__Tapeline__/tool"))
-handler.add_lib(require("__Tapeline__/tape"))
+handler.add_lib(require("__Tapeline__/scripts/tool"))
+handler.add_lib(require("__Tapeline__/scripts/tape"))
